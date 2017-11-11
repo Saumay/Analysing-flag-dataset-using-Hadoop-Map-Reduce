bf2ff74ef76e3fa7ac668a0fec1a9ae60da869e2
Saumay <khandelwalsaumay@gmail.com>
2017-11-12 04:54:31 +0530

    mapper file added

---
+++ area/mapper.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#for landmass
+
+import sys
+
+# input comes from STDIN (standard input)
+def main():
+    for line in sys.stdin:
+        # remove leading and trailing whitespace
+        line = line.strip()
+        # split the line into words
+        words = line.split(",")
+        
+	country = words[0]
+
+        print '%s\t%s' % (country)
+
+main()
