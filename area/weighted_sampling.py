7d9b850ca91c9659b405e15b848b9d4aa8c2575f
Saumay <khandelwalsaumay@gmail.com>
2017-11-14 22:06:11 +0530

    weighted sampling working fine for every test case

--- area/weighted_sampling.py
+++ area/weighted_sampling.py
@@ -18,9 +18,9 @@ def choice(population, weights):
     idx = bisect.bisect(cdf_vals, x)
     return population[idx]
 
-weights=[0.5, 0.6, 0.1]
+weights=[0.2, 0.4, 0.1,0.3]
 
-population = 'ABC'
+population = 'ABCD'
 counts = collections.defaultdict(int)
 for i in range(10000):
     print(choice(population, weights))
