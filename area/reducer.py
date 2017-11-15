b9abe3c441526d3b5975a8749b9d478fb6220e37
Saumay <khandelwalsaumay@gmail.com>
2017-11-15 14:07:38 +0530

    output data received from mapper

---
+++ area/reducer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from operator import itemgetter
+import sys
+import random
+import bisect
+import collections  
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+
+def MAIN():
+	# input comes from STDIN
+		for line in sys.stdin:
+			# remove leading and trailing whitespace
+			line = line.strip()
+			words = line.split("\t")
+			# parse the input we got from mapper.py
+			current_country = words[0]
+			current_feature = words[1]
+
+MAIN()
