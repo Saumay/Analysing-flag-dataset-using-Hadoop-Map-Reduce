77a522de15880348bb64324f24b4cfe4720cf4a8
Saumay <khandelwalsaumay@gmail.com>
2017-11-16 18:18:05 +0530

    function created for computing cenetroids

--- area/reducer.py
+++ area/reducer.py
@@ -11,6 +11,39 @@ from matplotlib.widgets import Slider
 
 features = []
 countries = []
+def compute_centeroids(k):
+	weights = []
+	c1 = random_sampling(features)
+	centeroids.append(c1)
+	index1 = features.index(c1)
+	country1 = countries[index1]
+	print 'c1=%d,country=%s' % (c1,country1)
+	centeroids_countries.append(country1)
+	for i in range(0,k-1):
+		for j in range(0,len(features)):
+			small = max(features)
+			for l in range(0,len(centeroids)):
+				di = pow((int(features[j]) - centeroids[l]),2)
+				#print 'di=%d'%(di)
+				if(di<small):
+					small = di
+			di_final = small
+			#print 'di_final=%d'%(di_final)
+			#print()
+			weights.append(pow(di_final,2))
+		sum_weights = sum(weights)
+		weights = [float(x)/sum_weights for x in weights]
+		#print("Weights",weights)
+		cn = weighted_sampling(weights, features)
+		centeroids.append(cn)  
+
+		indexn = features.index(cn)
+		countryn = countries[indexn]
+		centeroids_countries.append(countryn)  
+
+		weights=[]   
+		#print '%d:%s' % (cn,countryn)  
+	print("\n")     
 
 def MAIN():
 	# input comes from STDIN
