b29952eb23e0d3ea27aef595329eb4484d3be7ba
Saumay <khandelwalsaumay@gmail.com>
2017-11-16 18:17:03 +0530

    countries and features being stored in a list

--- area/reducer.py
+++ area/reducer.py
@@ -9,6 +9,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 
+features = []
+countries = []
+
 def MAIN():
 	# input comes from STDIN
 		for line in sys.stdin:
@@ -18,5 +21,8 @@ def MAIN():
 			# parse the input we got from mapper.py
 			current_country = words[0]
 			current_feature = words[1]
+			#print '%s,%s' % (current_country, current_feature)
+			features.append(int(current_feature))
+			countries.append(current_country)
 
 MAIN()
