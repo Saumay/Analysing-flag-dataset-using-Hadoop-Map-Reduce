37ba8ea05855834a738827e6253c6a9324cecb78
Saumay <khandelwalsaumay@gmail.com>
2017-11-25 11:54:06 +0530

    reducer code to print input from mapper

---
+++ population/reducer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from operator import itemgetter
+import sys
+import random
+import bisect
+import collections  
+import numpy as np
+import matplotlib.pyplot as plt
+		
+def MAIN():
+	# input comes from STDIN
+		for line in sys.stdin:
+			# remove leading and trailing whitespace
+			line = line.strip()
+			words = line.split("\t")
+			# parse the input we got from mapper.py
+			current_country = words[0]
+			current_feature = words[1]
+			print '%s,%s' % (current_country, current_feature)
+
+MAIN()
