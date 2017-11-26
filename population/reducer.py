3c887e2fb005f6b2559393db9a8ce2ed9700fe3b
Saumay <khandelwalsaumay@gmail.com>
2017-11-26 06:55:03 +0530

    weighted sampling added in reducer

--- population/reducer.py
+++ population/reducer.py
@@ -11,6 +11,37 @@ import matplotlib.pyplot as plt
 features = []
 countries = []
 
+def cdf(weights):
+	total = sum(weights)
+	result = []
+	cumsum = 0
+	for w in weights:
+		cumsum += w
+		result.append(cumsum / total)
+	#print(cumsum,total)
+	return result
+
+def choice(population, weights):
+	assert len(population) == len(weights)
+	cdf_vals = cdf(weights)
+	x = random.random()
+	idx = bisect.bisect(cdf_vals, x)
+	#print(population[idx])
+	return population[idx]
+
+def weighted_sampling(weights, population):
+	#weights=[0.1, 0.8, 0.1]
+	#population = 'ABC'
+	counts = collections.defaultdict(int)
+	#print(choice(population, weights))
+	return choice(population, weights)
+	#print(counts)
+	"""testing
+	for i in range(100):
+		print(choice(population, weights))
+		counts[choice(population, weights)] += 1
+	"""
+
 def MAIN():
 	# input comes from STDIN
 		for line in sys.stdin:
