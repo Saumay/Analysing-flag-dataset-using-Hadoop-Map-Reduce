70830f30ff5c14eb54f1c2049986754469c6037e
Saumay <khandelwalsaumay@gmail.com>
2017-11-27 17:06:16 +0530

    function added for computing centeroids

--- population/reducer.py
+++ population/reducer.py
@@ -11,6 +11,9 @@ import matplotlib.pyplot as plt
 features = []
 countries = []
 
+centeroids = []
+centeroids_countries = []
+
 def cdf(weights):
 	total = sum(weights)
 	result = []
@@ -42,6 +45,41 @@ def weighted_sampling(weights, population):
 		counts[choice(population, weights)] += 1
 	"""
 
+##CALCULATING CENTRES OF CENTEROIDS
+def compute_centeroids(k):
+	weights = []
+	c1 = random_sampling(features)
+	centeroids.append(c1)
+	index1 = features.index(c1)
+	country1 = countries[index1]
+	#print 'c1=%d,country=%s' % (c1,country1)
+	centeroids_countries.append(country1)
+	for i in range(0,k-1):
+		for j in range(0,len(features)):
+			small = 0
+			for l in range(0,len(centeroids)):
+				di = pow((int(features[j]) - centeroids[l]),2)
+				#print 'di=%d'%(di)
+				if(di<small):
+					small = di
+			di_final = small
+			#print 'di_final=%d'%(di_final)
+			#print()
+			weights.append(pow(di_final,2))
+		sum_weights = sum(weights)
+		weights = [float(x)/sum_weights for x in weights]
+		#print("Weights",weights)
+		cn = weighted_sampling(weights, features)
+		centeroids.append(cn)  
+
+		indexn = features.index(cn)
+		countryn = countries[indexn]
+		centeroids_countries.append(countryn)  
+
+		weights=[]   
+		#print '%d:%s' % (cn,countryn)  
+	print("\n")     
+
 def MAIN():
 	# input comes from STDIN
 		for line in sys.stdin:
@@ -54,5 +92,10 @@ def MAIN():
 			print '%s,%s' % (current_country, current_feature)
 			features.append(int(current_feature))
 			countries.append(current_country)
+		
+		#print '%d' % (features_sum)  
+		k=5
+
+		compute_centeroids(k)
 
 MAIN()
